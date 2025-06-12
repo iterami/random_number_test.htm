93b052ea63e0318d4178873cda336b67315f71ca
Jan Hořava <czechinthemail@gmail.com>
2025-06-12 11:53:19 -0700

    updated core_random_integer() usage

--- repo.js
+++ repo.js
@@ -4,9 +4,7 @@ function random_number_test(){
     let line = '';
 
     for(let i = 0; i < 50; i++){
-        line += String.fromCharCode(core_random_integer({
-          'max': 42,
-        }) + 48);
+        line += String.fromCharCode(core_random_integer(42) + 48);
     }
 
     line += '\n';
